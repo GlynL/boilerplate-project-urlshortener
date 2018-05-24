5a7623d76ce120d0423ffaf8eb93ec8ad9811eae
Glyn L <glyn_92@hotmail.com>
2018-05-24 14:09:53 +0700

    initial commit

--- server.js
+++ server.js
@@ -1,37 +1,60 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+var express = require("express");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
 
-var cors = require('cors');
+var cors = require("cors");
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
+mongoose.set("debug", true);
+mongoose.connect("mongodb://localhost/urlshortener", { useMongoClient: true });
+mongoose.Promise = Promise;
 // mongoose.connect(process.env.MONGOLAB_URI);
+
+var urlSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: "url cannot be blank"
+  }
+});
+
+var Url = mongoose.model("Url", urlSchema);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "hello API" });
 });
 
+app.post("/api/shorturl/new", function(req, res) {
+  db.Url.create(req.body)
+    .then(newUrl => res.json(newUrl))
+    .catch(err => console.log(err));
+  // res.json({
+  // url: req.body.url,
+  // short_url: "placeholder"
+  // });
+});
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+app.listen(port, function() {
+  console.log("Node.js listening ...");
 });
